5274c6dc052690a8b29212035ed37ef9afcd8d12
Hasnain Mucklai <mucklaihasnain@gmail.com>
2024-04-27 10:37:48 -0700

    completed homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import HomePage from './components/HomePage'; 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <div>
+    <HomePage /> {}
+  </div>
+);
 
-  return (
-    
-    <BrowserRouter>
-  
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      
-    </BrowserRouter>
-  )
-}
-
-export default App
+export default App;
