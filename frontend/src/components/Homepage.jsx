21b38cea1a6f342ddf19aa3eed058777413e8d30
Trung Tran <trungtran63470@gmail.com>
2024-04-28 15:01:51 -0700

    added Celia AI

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Pagination,
   PaginationItem,
   Typography,
   TextField,
-  Button
+  Button, 
+  Alert
 } from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -14,15 +15,68 @@ import { signOut } from "firebase/auth";
 import { ReactMediaRecorder } from "react-media-recorder";
 import Recorder from "./Recorder";
 import axios from "axios";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Homepage() {
   const [currentMessage, setCurrentMessage] = useState(''); 
   const [audioBlob, setAudioBlob] = useState(null); 
   const [messages, setMessages] = useState([]);
+  const [botMessages, setBotMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [jobDescription, setJobDescription] = useState('');
+  const [response, setResponse] = useState(null); 
+  const [user] = useAuthState(auth);
+  const [showAlert, setShowAlert] = useState(false);
+  const [totalMessages, setTotalMessages] = useState([]);
+
+  
+  const prevMessages = useRef([]);
+  const prevBotMessages = useRef([]);
+
+    
+
+  useEffect(() => {
+    // Determine which dependency changed
+    const changedMessages = messages !== prevMessages.current ? messages : null;
+    const changedBotMessages = botMessages !== prevBotMessages.current ? botMessages : null;
+
+    // Process the changed message
+    let lastMessage = null;
+    if (changedMessages !== null) {
+        // Handle changed messages
+        console.log('Messages changed:', changedMessages);  
+        lastMessage = changedMessages.slice(-1)[0];
+    }
+    if (changedBotMessages !== null) {
+        // Handle changed bot messages
+        console.log('Bot messages changed:', changedBotMessages);
+        lastMessage = changedBotMessages.slice(-1)[0];
+        receiveMessageAudioOutput(lastMessage);
+    }
+
+    if (lastMessage !== null) {
+        setTotalMessages(prevTotalMessages => [...prevTotalMessages, lastMessage]);
+    }
+
+    // Update previous values for the next comparison
+    prevMessages.current = messages;
+    prevBotMessages.current = botMessages;
+
+}, [messages, botMessages]);
+
+
+  useEffect(() => {
+    if (jobDescription != "") {
+      messageResponseFunction();
+    }
+  }, [messages])
 
   const submitJobDescription = () => {
+    if (jobDescription.trim() === '') {
+      // Show an alert or toast message indicating that the field is required
+      setShowAlert(true);
+      return; // Do not proceed with submission
+    }
     axios.post('http://localhost:8000/submit_job_description', {
       description: jobDescription}).then((response) => {
         console.log(jobDescription);
@@ -41,9 +95,9 @@ function Homepage() {
       .then(async (audioBlob) => {
         const formData = new FormData();
         formData.append("file", audioBlob, "audio.wav");
-        await axios.post("http://localhost:8000/post_audio", formData, { headers: { 'Content-Type': 'multipart/form-data' }, responseType: 'arraybuffer' })
+        await axios.post("http://localhost:8000/post_audio_and_get_text", formData, { headers: { 'Content-Type': 'multipart/form-data' }})
           .then((response) => {
-            const blob = response.data;
+            /*const blob = response.data;
             const audio = new Audio();
             audio.src = createBlobURL(blob);
             const responseAudio = { sender: "Celia", audioBlob: audio.src };
@@ -51,6 +105,8 @@ function Homepage() {
             setMessages(messageList);
             setIsLoading(false);
             audio.play();
+            */  
+            setMessages([...messages, user.displayName + ": " + response.data.response])
           })
           .catch((error) => {
             console.error("Error:", error);
@@ -58,70 +114,70 @@ function Homepage() {
       });
   };
 
+  const messageResponseFunction = async () => {
+    const messageInput = totalMessages.toString();
+    console.log(messageInput);
+    const response = await axios.post("http://localhost:8000/respond_message", { input_message: messageInput });
+    console.log(response.data)
+    setBotMessages([...botMessages, "Celia: " +  response.data])
+  }
+
   function createBlobURL(data) {
     const blob = new Blob([data], { type: "audio/mpeg" });
     const url = window.URL.createObjectURL(blob);
     return url;
   }
 
+  const receiveMessageAudioOutput = async (input) => {
+    await axios.post("http://localhost:8000/text_to_speech", {input_message: input}, { responseType: 'arraybuffer' })
+    .then((response) => {
+      const blob = response.data;
+      const audio = new Audio();
+      audio.src = createBlobURL(blob);
+      setIsLoading(false);
+      console.log("audio ", audio);
+      audio.play(); 
+    })
+    .catch((error) => { 
+      console.error("Error:", error);
+    });
+  }
   return (
     <div className="w-full min-h-screen flex items-center justify-center gap-4">
-        <div className="w-1/5 h-96 rounded-2xl drop-shadow-lg bg-slate-100 relative p-4">
+        <div className="w-2/5 h-128 rounded-2xl drop-shadow-lg bg-slate-100 relative p-4">
             <h1 className="font-bold text-2xl">Job Description</h1>
             <TextField
           sx={{ width: "100%", mt: 1 }}
           id="response"
-          label="Job Description"
+          placeholder="Job Description"
           onChange={(e) => setJobDescription(e.target.value)}
-          multiline
-          rows={10}
-          defaultValue="Paste job description here..."
+          multiline 
+          rows={26}
           inputProps={{ style: { fontSize: "0.8rem" } }}
         />
+       
         <div className="w-full flex justify-center">
           <Button onClick={submitJobDescription} sx={{mt: 1}}variant="contained">Submit</Button>
         </div>
+        {showAlert && (
+          <Alert severity="error" sx={{ mt: 1 }}>
+            Please enter the job description.
+          </Alert>
+        )}
 
         </div>
-      <div className="w-1/3 h-70 rounded-2xl drop-shadow-lg bg-slate-100 relative p-4">
-        <Typography sx={{ fontWeight: "bold" }}>
-          Question: Tell me about yourself
-        </Typography>
-        <TextField
-          sx={{ width: "100%", mt: 1 }}
-          id="response"
-          label="Response"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-          inputProps={{ style: { fontSize: "0.8rem" } }}
-        />
-
-        <Pagination
-          sx={{
-            position: "absolute",
-            bottom: 3,
-            left: 0,
-            right: 0,
-            display: "flex",
-            justifyContent: "center",
-          }}
-          count={5}
-          renderItem={(item) => (
-            <PaginationItem
-              slots={{
-                previous: ArrowBackIosNewIcon,
-                next: ArrowForwardIosIcon,
-              }}
-              {...item}
-            />
-          )}
-        />
-
-        <div>
+      <div className="w-1/3 p-4 h-128 rounded-2xl drop-shadow-lg bg-slate-100 relative overflow-y-auto gap-4 flex flex-col">
+        { totalMessages.map((message) => (
+          <div className="p-2 bg-slate-300 drop-shadow-lg rounded-2xl">
+            <Typography sx={{ fontWeight: "bold" }}>
+              {message}
+            </Typography>
+        </div>
+        ))}
+      </div>
+      <div className="ml-30">
           <Recorder handleStop={saveAudio} />
         </div>
-      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '48rem',
+      }
+    },
   },
   plugins: [],
 }
