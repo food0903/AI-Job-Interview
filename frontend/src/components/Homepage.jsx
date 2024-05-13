9a1732f21162090738019c6f321d849b5fb127b7
Ryan <rqn.pham@gmail.com>
2024-05-13 14:05:01 -0700

    add animation for ai chat

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -19,7 +19,7 @@ import axios from "axios";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Sidebar from "./Sidebar";
 import SidebarLayout from "./SidebarLayout";
-
+import { motion } from "framer-motion"
 function Homepage() {
   const [currentMessage, setCurrentMessage] = useState('');
   const [audioBlob, setAudioBlob] = useState(null);
@@ -218,17 +218,25 @@ function Homepage() {
                     totalMessages.map((message) => (
                       <>
                         {message.role === "User" &&
-                          <div className="flex flex-row justify-end gap-x-2 items-center">
+                          <motion.div 
+                          initial={{ x: '80vw' }}
+                          animate={{ x: 0 }}
+                          transition={{ type: 'tween', duration: 0.25 }}
+                          className="flex flex-row justify-end gap-x-2 items-center">
                             <div className="p-2 bg-slate-200 max-w-[800px] drop-shadow-lg rounded-2xl">
                               <Typography sx={{ fontFamily: "nunito" }}>
                                 {message.content}
                               </Typography>
                             </div>
                             <Avatar src={user.photoURL} sx={{ bgcolor: "purple" }}></Avatar>
-                          </div>
+                          </motion.div>
                         }
                         {message.role === "Celia" &&
-                          <div className="flex flex-row justify-start gap-x-2 items-center">
+                          <motion.div 
+                          initial={{ x: '-20vw' }}
+                          animate={{ x: 0 }}
+                          transition={{ type: 'tween', duration: 0.25 }}
+                          className="flex flex-row justify-start gap-x-2 items-center">
                             <Avatar src="/Celia.jpg" sx={{ bgcolor: "purple" }}></Avatar>
                             <div className="p-2 bg-blue-500 max-w-[800px] drop-shadow-lg rounded-2xl">
                               <Typography sx={{ color: "white", fontFamily: "nunito" }}>
@@ -236,7 +244,7 @@ function Homepage() {
                               </Typography>
                             </div>
 
-                          </div>
+                          </motion.div>
                         }
 
 
