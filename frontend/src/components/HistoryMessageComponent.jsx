a5596d8f38ab36438c65ffe8eb6f4b8b06542bae
Ryan <rqn.pham@gmail.com>
2024-05-13 10:09:55 -0700

    make history message component clickable

--- frontend/src/components/HistoryMessageComponent.jsx
+++ frontend/src/components/HistoryMessageComponent.jsx
@@ -1,7 +1,7 @@
 
 export default function HistoryMessageComponent({sid, setSidState, jobDescription}) {
     return (
-        <div onClick={() => setSidState(sid)} className="w-full h-24 border-1 overflow-hidden font-nunito p-2">
+        <div onClick={() => setSidState(sid)} className="hover:cursor-pointer w-full h-24 border-1 overflow-hidden font-nunito p-2">
             <p className="line-clamp-2">{jobDescription}</p>
         </div>
     );
