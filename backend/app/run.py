ea3775febb0c85e5cade204c6be5f0cab0417213
Ryan <rqn.pham@gmail.com>
2024-05-18 14:59:24 -0700

    fix

--- backend/app/run.py
+++ backend/app/run.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Add the parent directory of 'app' to the PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
