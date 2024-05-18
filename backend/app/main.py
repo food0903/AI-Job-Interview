4a85a7477fb0af9af9db8f1ebb90453a4163ec16
Ryan <rqn.pham@gmail.com>
2024-05-18 15:52:05 -0700

    fix cors, .env, and .gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,17 +15,9 @@ import os
 # FastAPI instance
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5173"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
